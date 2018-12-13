bc9aac4512b5796098eb52b56f66c2389822d69f
Ines Chami <ineschami@hotmail.com>
2018-12-12 19:31:05 -0800

    added scripts

---
+++ pygcn/train_lp.py
@@ -0,0 +1,128 @@
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import time
+
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.optim as optim
+
+sys.path.append('/Users/ineschami/PycharmProjects/gae-pytorch/')
+import pygcn.models as models
+from pygcn.utils.data_utils import load_data, preprocess_graph, mask_l_u_edges
+from pygcn.utils.eval_utils import get_roc_score, sigmoid
+from pygcn.utils.train_utils import loss_function_with_masking as loss_function
+
+# from gae.utils import load_data, preprocess_graph, mask_test_edges
+# from gae.optimizer import loss_function
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=10000,
+                    help='Number of epochs to train.')
+parser.add_argument('--patience', type=int, default=20,
+                    help='Patience for early stopping.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=0,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=32,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--dataset', type=str, default='cora',
+                    help='Dataset to use.')
+parser.add_argument('--model', type=str, default='VGAE',
+                    help='Model to use.')
+parser.add_argument('--save', action='store_true', default=False,
+                    help='Whether to save the adjacency scores.')
+
+args = parser.parse_args()
+
+
+def gae_for(args):
+    print("Using {} dataset".format(args.dataset))
+    adj, features, _, l_indices, _, _ = load_data(dataset_str=args.dataset)
+    features = torch.FloatTensor(np.array(features.todense()))
+    n_nodes, feat_dim = features.shape
+    u_indices = [i for i in range(n_nodes) if i not in l_indices]
+    # Store original adjacency matrix (without diagonal entries) for later
+    adj_orig = adj
+    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+    adj_orig.eliminate_zeros()
+
+    adj_train, adj_mask, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_l_u_edges(
+            adj, l_indices, u_indices)
+    adj = adj_train
+    adj_mask = torch.FloatTensor(adj_mask)
+
+    # Some preprocessing
+    adj_norm = preprocess_graph(adj)
+    adj_label = adj_train + sp.eye(adj_train.shape[0])
+    # adj_label = sparse_to_tuple(adj_label)
+    adj_label = torch.FloatTensor(adj_label.toarray())
+
+    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
+    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
+    model_class = getattr(models, args.model)
+    model = model_class(feat_dim, args.hidden, args.dropout)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    hidden_emb = None
+    counter = 0
+    best_val_ap = -1
+    best_val_roc = -1
+    roc_score = None
+    ap_score = None
+    for epoch in range(args.epochs):
+        t = time.time()
+        model.train()
+        optimizer.zero_grad()
+        recovered, mu, logvar = model(features, adj_norm)
+        loss = loss_function(preds=recovered, labels=adj_label, adj_mask=adj_mask,
+                             mu=mu, logvar=logvar, n_nodes=n_nodes,
+                             norm=norm, pos_weight=pos_weight)
+        loss.backward()
+        cur_loss = loss.item()
+        optimizer.step()
+
+        hidden_emb = mu.data.numpy()
+        roc_curr, ap_curr = get_roc_score(hidden_emb, adj_orig, val_edges, val_edges_false)
+
+        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(cur_loss),
+              "val_ap=", "{:.5f}".format(ap_curr),
+              "time=", "{:.5f}".format(time.time() - t)
+              )
+        if ap_curr > best_val_ap:  # or roc_curr > best_val_roc:
+            if ap_curr > best_val_ap:  # and roc_curr > best_val_roc:
+                roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
+            if args.save:
+                np.save(os.path.join('reconstructions', '{}-{}-{}'.format(args.dataset, args.model, args.hidden)),
+                        sigmoid(hidden_emb.dot(hidden_emb.T)), )
+            best_val_ap = ap_curr
+            best_val_roc = roc_curr
+            counter = 0
+        else:
+            counter += 1
+            if counter == args.patience:
+                print("Early stopping.")
+                break
+
+    print("Optimization Finished!")
+    if not roc_score:
+        roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
+    print('Test ROC score: ' + str(roc_score))
+    print('Test AP score: ' + str(ap_score))
+
+
+if __name__ == '__main__':
+    gae_for(args)
---
+++ pygcn/train_nc.py
@@ -0,0 +1,140 @@
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import time
+
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+import pygcn.models as models
+from pygcn.utils.data_utils import load_data, accuracy, normalize, sparse_mx_to_torch_sparse_tensor, mask_l_u_edges
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=10000,
+                    help='Number of epochs to train.')
+parser.add_argument('--patience', type=int, default=100,
+                    help='Patience for early stopping.')
+parser.add_argument('--lr', type=float, default=0.005,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=128,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.6,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--dataset', type=str, default='cora',
+                    help='Dataset to use.')
+parser.add_argument('--model', type=str, default='GCN',
+                    help='Model to use.')
+parser.add_argument('--drop-prop', type=float, default=0,
+                    help='Proportion of edges to remove.')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+# np.random.seed(args.seed)
+# torch.manual_seed(args.seed)
+# if args.cuda:
+#     torch.cuda.manual_seed(args.seed)
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data(dataset_str=args.dataset)
+features = normalize(features)
+features = torch.FloatTensor(np.array(features.todense()))
+labels = torch.LongTensor(labels)
+
+l_indices = idx_train
+u_indices = [i for i in range(adj.shape[0]) if i not in l_indices]
+adj, adj_mask, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_l_u_edges(
+        adj, l_indices, u_indices)
+adj = normalize(adj + sp.eye(adj.shape[0]))
+adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+# Model and optimizer
+model_class = getattr(models, args.model)
+model = model_class(nfeat=features.shape[1],
+                    nhid=args.hidden,
+                    nclass=labels.max().item() + 1,
+                    dropout=args.dropout)
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+
+def test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    return loss_test, acc_test
+
+
+# Train model
+t_total = time.time()
+counter = 0
+best_val_acc = -1
+best_val_loss = np.inf
+best_loss_test = None
+best_acc_test = None
+for epoch in range(args.epochs):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj)
+    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+
+    if not args.fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        output = model(features, adj)
+
+    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+    print('Epoch: {:04d}'.format(epoch + 1),
+          'loss_train: {:.4f}'.format(loss_train.item()),
+          'acc_train: {:.4f}'.format(acc_train.item()),
+          'loss_val: {:.4f}'.format(loss_val.item()),
+          'acc_val: {:.4f}'.format(acc_val.item()),
+          'time: {:.4f}s'.format(time.time() - t))
+    if acc_val > best_val_acc or loss_val < best_val_loss:
+        if acc_val > best_val_acc and loss_val < best_val_loss:
+            best_loss_test, best_acc_test = test()
+        counter = 0
+        best_val_acc = acc_val
+        best_val_loss = loss_val
+    else:
+        counter += 1
+        if counter == args.patience:
+            print("Early stopping")
+            break
+
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+if not best_loss_test:
+    # Testing
+    best_loss_test, best_acc_test = test()
+print("Test set results:",
+      "loss= {:.4f}".format(best_loss_test.item()),
+      "accuracy= {:.4f}".format(best_acc_test.item()))
